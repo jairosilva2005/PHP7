833915d3de6641638af3bd01500739374e31a1ba
Jairo Silva <70125199+jairosilva2005@users.noreply.github.com>
2021-01-28 22:16:24 -0300

    Add files via upload

---
+++ cursophp.com/11-Operadores-aritmeticos.php
@@ -0,0 +1,43 @@
+<?php
+	// Operadores aritméticos em PHP seguem um padrão parecido com JavaScript!
+	/*
+	Operador de adição: +
+	Operador de subtração: -
+	Operador de divisão: /
+	Operador de multiplicação: *
+	Operador de exponenciação: **
+	Operador resto da divisão: %
+	*/
+	// Exercício
+	$n1 = 10;
+	$n2 = 12;
+	
+	// Operador de adição:
+	$Conta_1 = $n1 + $n2; // Saída: 22
+	// Operador de subtração:
+	$Conta_2 = $n1 - $n2; // Saída: -2
+	// Operador de divisão:
+	$Conta_3 = $n1 / $n2; // Saída: 0,83
+	// Operador de multiplicação:
+	$Conta_4 = $n1 * $n2; // Saída: 120
+	// Operador de exponenciação:
+	$Conta_5 = $n1 ** $n2; // Saída: 1000000000000
+	// Operador de resto da divisão:
+	$Conta_6 = $n1 % $n2; // Saída: 10
+
+	echo "Exercício - Operadores Aritméticos<br>";
+	echo "<hr>";
+	echo "Resultado da adição: $Conta_1<br>";
+	echo "<hr>";	
+	echo "Resultado da subtração: $Conta_2<br>";
+	echo "<hr>";
+	echo "Resultado da divisão: $Conta_3<br>";
+	echo "<hr>";
+	echo "Resultado da multiplicação: $Conta_4<br>";
+	echo "<hr>";
+	echo "Resultado da exponenciação: $Conta_5<br>";
+	echo "<hr>";
+	echo "Resultado de resto da divisão: $Conta_6<br>";
+
+	// Fim do código
+?>
