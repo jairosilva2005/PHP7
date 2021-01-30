097ffd59aacfca5bf437292a4dc7dc734e592099
Jairo Silva <70125199+jairosilva2005@users.noreply.github.com>
2021-01-30 19:05:19 -0300

    Add files via upload

---
+++ cursophp.com/13-Operadores-Atribuicao.php
@@ -0,0 +1,12 @@
+<?php
+	// Operadores de atribuição
+	/* Operadores de atribuição podem facilitar e simplificar certas operações */
+	$x = 10;
+	$y = 12;
+
+	// $x = $x + $y; podemos simplificar esse tipo de operação usando as atribuições - Exemplo:
+	$x += $y; // Seria o mesmo que digitar $x = $x + $y
+	$x -= $y; // Seria o mesmo que digitar $x = $x - $y
+	$x /= $y; // Seria o mesmo que digitar $x = $x / $y
+	$x *= $y; // Seria o mesmo que digitar $x = $x * $y
+	$x %= $y; // Seria o mesmo que digitar $x = $x % $y
