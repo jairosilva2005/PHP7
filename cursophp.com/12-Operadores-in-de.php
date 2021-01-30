710c7111adcf32bd0b91062de8e946e77d48ff19
Jairo Silva <70125199+jairosilva2005@users.noreply.github.com>
2021-01-30 19:06:21 -0300

    Add files via upload

---
+++ cursophp.com/12-Operadores-in-de.php
@@ -0,0 +1,13 @@
+<?php
+	// Operadores de incremento e decremento servem para somar em +1 ou subtrair em -1
+	$Teste = 10;
+
+	// Operador de pré-incremento
+	++$Teste;
+	// Operador de pós-decremento
+	$Teste++;
+
+	// Operador de pré-decremento
+	--$Teste;
+	// Operador de pós-decremento
+	$Teste--;
